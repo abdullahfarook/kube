4489e91c7729336e9d328ad0675b406737c53138
Abdullah <abdullah@otaskies.com>
2024-08-17 03:11:05 +0500

    fixes

--- attach-disk.ps1
+++ attach-disk.ps1
@@ -15,8 +15,8 @@ $disk = $disk -split " " | Where-Object { $_ -ne "" }
 $disk = $disk[0]
 Write-Output "Disk found: $disk"
 $partition = $disk + "1"
-if ($existing -eq $false) {
-    # partition the disk
+if (($existing -eq $false) -and (-not (lsblk -o NAME,SIZE,MOUNTPOINT | grep -i "$disk" | grep -i "part"))) {
+    # check if disk is already partitioned then do not partition
     parted /dev/$disk --script mklabel gpt mkpart xfspart xfs 0% 100%
     mkfs.xfs /dev/$partition
     partprobe /dev/$partition
@@ -30,7 +30,7 @@ mount /dev/$partition $path
 $uuid = blkid | grep -i $partition | ForEach-Object { $_ -split " " } | Where-Object { $_ -match "UUID" }
 $uuid = $uuid -split "=" | Where-Object { $_ -ne "" }
 $uuid = $uuid[1]
-# if UUID exists in fstab, does not add with new line at start
+# if UUID exists in fstab, does not add
 if ($null -eq (grep -i $uuid /etc/fstab)) {
     Add-Content /etc/fstab "UUID=$uuid   $path   xfs   defaults,nofail   1   2"
 }
