aa6be5193a6523559f818791f59167626b9565e0
Abdullah <abdullah@otaskies.com>
2024-08-17 04:08:58 +0500

    refs

--- mysql.ps1
+++ mysql.ps1
@@ -1,31 +1,36 @@
+# sudo pwsh -Command "iex '& ([scriptblock]::Create((iwr https://raw.githubusercontent.com/abdullahfarook/kube/main/mysql.ps1))) -mysql_root_password 'password' "
 Param(
     [string]$mysql_root_password,
     [string]$mysql_path = "/shared/mysql",
     [string]$mysql_version = "8.0.39",
-    [string]$join_network = "shared_network",
+    [string]$join_network,
     [bool]$existing = $true,
-    [string]$new_user = "user",
-    [string]$new_password = "Pass"
+    [string]$new_user,
+    [string]$new_password
 )
 
 $compose = @"
-version: '3'
-services:
-  mysql:
-    image: mysql/mysql-server:$mysqlVersion
+mysql:
+    image: mysql/mysql-server:${mysqlVersion}
     container_name: mysql
     volumes:
-      - $mysqlPath/data:/var/lib/mysql
-      - $mysqlPath/conf.d:/etc/mysql/conf.d
+      - ${mysqlPath}/data:/var/lib/mysql
+      - ${mysqlPath}/conf.d:/etc/mysql/conf.d
     restart: always
     ports:
       - 3306:3306
     environment:
-      MYSQL_ROOT_PASSWORD: $mysqlRootPassword
+      MYSQL_ROOT_PASSWORD: ${mysqlRootPassword}
 "@
 $file = "mysql-compose.yml"
 $compose | Out-File -FilePath $file -Force
-nerdctl compose -f $file up -d
+
+try {
+    nerdctl compose -f $file up -d
+} catch {
+    Write-Error "Failed to start MySQL container:`n$_"
+    exit 1
+}
 
 # join network
 if ($null -ne $joinNetwork) {
@@ -36,6 +41,11 @@ if ($null -ne $joinNetwork) {
 
 # create user and database
 if ($existing -eq $false) { 
+  #check for arguments new_user and new_password
+  if ($null -eq $newUser -or $null -eq $newPassword) {
+    Write-Error "new_user and new_password must be provided if provided argument 'existing' is false"
+    exit 1
+  }
 $command = @"
 CREATE USER '$newUser'@'%' IDENTIFIED WITH mysql_native_password BY '$newPassword';
 GRANT ALL PRIVILEGES ON *.* TO '$newUser'@'%';
