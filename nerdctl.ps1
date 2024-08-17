fd8397c7d1f49c0dc710b27792d1ed6d819ce9ba
Abdullah <abdullah@otaskies.com>
2024-08-17 05:02:21 +0500

    refs

--- nerdctl.ps1
+++ nerdctl.ps1
@@ -1,20 +1,24 @@
 # download and extract containerd
+echo "Installing containerd..."
 wget https://github.com/containerd/containerd/releases/download/v1.7.20/containerd-1.7.20-linux-amd64.tar.gz
 tar Cxzvf /usr/local containerd-1.7.20-linux-amd64.tar.gz
 rm containerd-1.7.20-linux-amd64.tar.gz
 
 # download and extract cni plugins
+echo "Installing CNI plugins..."
 mkdir -p -m 755 /opt/cni/bin
 wget https://github.com/containernetworking/plugins/releases/download/v1.5.1/cni-plugins-linux-amd64-v1.5.1.tgz
 tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.5.1.tgz
 rm cni-plugins-linux-amd64-v1.5.1.tgz
 
 # download ans install runc
+echo "Installing runc..."
 wget https://github.com/opencontainers/runc/releases/download/v1.1.4/runc.amd64
 install -m 755 runc.amd64 /usr/local/sbin/runc
 rm runc.amd64
 
 # configure containerd
+echo "Configuring containerd..."
 mkdir -m 755 /etc/containerd
 containerd config default | tee /etc/containerd/config.toml
 sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
@@ -26,8 +30,13 @@ systemctl enable --now containerd
 systemctl status containerd
 
 # download and install nerdctl
+echo "Installing nerdctl..."
 wget https://github.com/containerd/nerdctl/releases/download/v1.7.6/nerdctl-1.7.6-linux-amd64.tar.gz
 tar -zxf  nerdctl-1.7.6-linux-amd64.tar.gz nerdctl
 mv nerdctl /usr/local/bin
 rm nerdctl-1.7.6-linux-amd64.tar.gz
 nerdctl --version
+
+# download and install iptables
+echo "Installing iptables..."
+apt install -y iptables
