0dc3851f826e0975e9b16853ebbcf2d3a2275357
Abdullah <abdullah@otaskies.com>
2024-08-24 19:48:19 +0500

    fixes

--- firewalls.ps1
+++ firewalls.ps1
@@ -3,13 +3,15 @@ apt update
 sudo apt-get --yes install firewalld
 
 echo "Opening ports 8472, 10250, 51820, 51821, 5001, 6443"
-firewall-cmd --zone=public --add-port=8472/udp --permanent `
-firewall-cmd --zone=public --add-port=10250/tcp --permanent  `
-firewall-cmd --zone=public --add-port=51820/udp --permanent `
-firewall-cmd --zone=public --add-port=51821/udp --permanent `
-firewall-cmd --zone=public --add-port=5001/tcp --permanent  `
-firewall-cmd --zone=public --add-port=6443/tcp --permanent `
-firewall-cmd --zone=public --add-port=80/tcp --permanent `
-firewall-cmd --zone=public --add-port=80/udp --permanent `
+firewall-cmd --zone=public --add-port=8472/udp --permanent
+firewall-cmd --zone=public --add-port=10250/tcp --permanent
+firewall-cmd --zone=public --add-port=51820/udp --permanent
+firewall-cmd --zone=public --add-port=51821/udp --permanent
+firewall-cmd --zone=public --add-port=5001/tcp --permanent
+firewall-cmd --zone=public --add-port=6443/tcp --permanent
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --zone=public --add-port=80/udp --permanent
+# firewall-cmd --permanent --zone=trusted --add-source=10.42.0.0/16
+# firewall-cmd --permanent --zone=trusted --add-source=10.43.0.0/16
 firewall-cmd --reload
 echo "Firewall configured successfully"
