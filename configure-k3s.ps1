b6128fad3cb848eba96d1bde115e614df6416d3c
Abdullah <abdullah@otaskies.com>
2024-08-24 21:26:24 +0500

    refs

--- configure-k3s.ps1
+++ configure-k3s.ps1
@@ -1,5 +1,5 @@
 # sudo pwsh -Command "iex '& ([scriptblock]::Create((iwr https://raw.githubusercontent.com/abdullahfarook/kube/main/configure-k3s.ps1).Content))'" 
 param (
     [bool]$disable_traefik = $true,
-    [bool]$taint_server = $true,
+    [bool]$taint_server = $true
 )
--- mysql.ps1
+++ mysql.ps1
@@ -64,7 +64,7 @@ CREATE USER '$new_user'@'%' IDENTIFIED WITH mysql_native_password BY '$new_passw
 GRANT ALL PRIVILEGES ON *.* TO '$new_user'@'%';
 FLUSH PRIVILEGES;
 "@
-    nerdctl exec mysql mysql -u root -p$mysqlRootPassword -e $command
+    nerdctl exec mysql mysql -u root -p$mysql_root_password -e $command
     Write-Host "New MySQL user $new_user created successfully."
 }
 
